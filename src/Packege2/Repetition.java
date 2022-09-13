bb79edb8b81b3c79971a791c8493a32758cd63b4
babanejad-m <m.b.sarabi@gmail.com>
2022-09-13 11:29:37 +0430

    MagicSquare

--- src/Packege2/Repetition.java
+++ src/Packege2/Repetition.java
@@ -7,13 +7,13 @@ public class Repetition {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
         System.out.println("Enter Number :");
-        int[] a = new int[10];
-        for (int i = 1; i <= 15; i++) {
-            int x = reader.nextInt();
-            a[x]++;
+        int[] a = new int[10];                       // ده عدد تک رقمی از کاربر گرفته و تعداد تکرار هر عدد چاپ شود
+        for (int i = 1; i <= 15; i++) {             //این حلقه تا گرفتن 15 عدد تک رقمی از عدد 1 توسط کاربر ادامه داشته باشه
+            int x = reader.nextInt();              // یک عدد از کاربر بگیر(کاربر شماره خانه را به ما میدهد )
+            a[x]++;                               // محتوای اون خونه را پلاس پلاس کن
         }
-        for (int i = 1; i < 10; i++) {
-            System.out.println(i + ": " + a[i]);
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i + ": " + a[i]);            // آی میشود شمارنده هر خانه و  آ آی  میشود تعداد تکرارها
         }
 //        Random rand = new Random();
 //        int[] a = new int[10];
